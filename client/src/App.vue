7969bee09faeca68b12efdd051849c3ffee881f5
mornir <jeromepott@hotmail.com>
2018-06-21 22:47:03 +0200

    Add wired-elements

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="my-container">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import { WiredButton, WiredInput } from 'wired-elements'
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    register() {},
+  },
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Amatic+SC:400,700');
+
+body {
+  font-family: 'Amatic SC', cursive;
+}
+
+.my-container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,48 @@
+<template>
+  <wired-card>
+    <div class="log-container">
+      <wired-input placeholder="Enter email" type="email" class="email"></wired-input>
+      <wired-input placeholder="Enter password" type="password"></wired-input>
+      <wired-input placeholder="Enter password" type="password" v-if="register"></wired-input>
+      <wired-button class="register" v-if="register">register</wired-button>
+      <wired-button class="register" v-else>login</wired-button>
+      <span class="alternative" @click="register = !register">Or {{ register ? 'register' : 'login'}}</span>
+    </div>
+  </wired-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      register: true,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.log-container {
+  margin: 2rem;
+  display: flex;
+  flex-direction: column;
+}
+
+.email {
+  margin-bottom: 0.5rem;
+}
+
+.register {
+  align-self: center;
+  margin: 1rem 0 0.5rem 0;
+}
+
+.alternative {
+  text-align: center;
+  cursor: pointer;
+}
+
+.alternative:hover {
+  text-decoration: underline;
+}
+</style>
